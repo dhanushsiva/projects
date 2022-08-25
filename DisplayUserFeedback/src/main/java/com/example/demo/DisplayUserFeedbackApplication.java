e5db7d3648219eb4e0c92324142690c537c99eb2
Dhanush S <dhanushsiva2000@gmail.com>
2022-08-25 18:42:15 +0530

    changes

---
+++ DisplayUserFeedback/src/main/java/com/example/demo/DisplayUserFeedbackApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@ComponentScan(basePackages="com")
+
+@EntityScan(basePackages="com")
+@EnableJpaRepositories(basePackages="com")
+public class DisplayUserFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisplayUserFeedbackApplication.class, args);
+	}
+
+}
